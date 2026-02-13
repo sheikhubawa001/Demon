d19ec9c313d3c310893fac6eb9fb4599fc9e1754
sheikhubawa001 <aftabsheikh2210@gmail.com>
2026-02-13 23:51:27 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+clear
+echo "Installing BL007 Speed tool...🔥"
+sleep 1
 
+chmod +x batman
+
+if [ -d "$PREFIX/bin" ]; then
+  mv batman $PREFIX/bin/
+else
+  echo "PREFIX/bin not found"
+  exit 1
+fi
+
+echo ""
+echo "Install Complete Bitcchh !!!😂 ✅"
+echo "Now type: batman"
